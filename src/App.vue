831b806612c55c4e6d9db223c770b38f3a236640
AnikHawk <altamashaqueanik@gmail.com>
2019-09-07 14:21:50 +0600

    Router Added

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,20 @@
 <template>
   <div id="app">
     <Header />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view />
   </div>
 </template>
 
 <script>
-import Header from "./components/Layout/Header";
-import Todos from "./components/Todos";
-
+import Header from './components/layout/Header';
 export default {
   name: "app",
   components: {
-    Header,
-    Todos
-  },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "VueJS Crash course",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "ML Course",
-          completed: false
-        },
-        {
-          id: 3,
-          title: "Neural Network Course",
-          completed: true
-        },
-        {
-          id: 4,
-          title: "Django Crash Course",
-          completed: false
-        }
-      ]
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id != id);
-    }
+    Header
   }
-};
+}
 </script>
+
 
 <style>
 * {
@@ -55,9 +22,19 @@ export default {
   margin: 0;
   padding: 0;
 }
-
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
 }
 </style>
