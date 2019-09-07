259b3e51d7b202ed54413f35062083907bce679b
AnikHawk <altamashaqueanik@gmail.com>
2019-09-07 12:48:37 +0600

    Todo Item and Header Added

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,63 @@
 <template>
   <div id="app">
-  <Todos v-bind:todos="todos"/>
+    <Header />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos'
+import Header from "./components/Layout/Header";
+import Todos from "./components/Todos";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
+    Header,
     Todos
   },
-  data(){
-    return{
+  data() {
+    return {
       todos: [
         {
           id: 1,
-          title: 'VueJS Crash course',
+          title: "VueJS Crash course",
           completed: false
         },
         {
           id: 2,
-          title: 'ML Course',
+          title: "ML Course",
           completed: false
         },
         {
           id: 3,
-          title: 'Neural Network Course',
+          title: "Neural Network Course",
           completed: true
         },
         {
           id: 4,
-          title: 'Django Crash Course',
+          title: "Django Crash Course",
           completed: false
-        },
-
+        }
       ]
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id != id);
     }
   }
-}
+};
 </script>
 
 <style>
- *{
-   box-sizing: border-box;
-   margin: 0;
-   padding: 0;
- }
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 
- body{
-   font-family: Arial, Helvetica, sans-serif;
-   line-height: 1.4;
- }
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
 </style>
