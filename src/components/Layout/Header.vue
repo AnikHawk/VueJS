e4e4c025120a28f967fb823e8ca800e7a01f5925
AnikHawk <altamashaqueanik@gmail.com>
2019-09-07 14:25:11 +0600

    Header Fixed

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -1,16 +1,18 @@
 <template>
-  <div>
-    <header class="header">Todo List</header>
-  </div>
+  <header class="header">
+    <h1>TodoList</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </header>
 </template>
-
 
 <script>
 export default {
   name: "Header"
-};
+}
 </script>
-
 
 <style scoped>
 .header {
